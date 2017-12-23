6a38b30bee17653a3f4e3f088a610470a87fac53
lolbot-iichan <lolbot_iichan@mail.ru>
2017-12-24 02:55:37 +0300

    Revert "sync"
    This reverts commit cdc09716b7a51dd94df958b6002ad65a3e5da015, reversing
    changes made to 3dcc4a0a4afda88af359f8af436eac5eeebcd2f0.

--- build/android/jni/lua/Android.mk
+++
@@ -1,6 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := lua
-LOCAL_SRC_FILES := ../../../../lib/android/lua/$(TARGET_ARCH_ABI)/liblua.so
-include $(PREBUILT_SHARED_LIBRARY)
--- build/android/jni/zlib/Android.mk
+++
@@ -1,6 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := z
-LOCAL_SRC_FILES := ../../../../lib/android/zlib/$(TARGET_ARCH_ABI)/libz.so
-include $(PREBUILT_SHARED_LIBRARY)
